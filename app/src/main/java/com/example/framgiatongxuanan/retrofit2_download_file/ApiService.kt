24fe7866b65bec9ddc242b3a19bc6119bc4eedb7
oTongXuanAn <tong.xuan.an@framgia.com>
2018-05-08 17:32:06 +0700

    code dao

--- app/src/main/java/com/example/framgiatongxuanan/retrofit2_download_file/ApiService.kt
+++ app/src/main/java/com/example/framgiatongxuanan/retrofit2_download_file/ApiService.kt
@@ -4,6 +4,7 @@ package com.example.framgiatongxuanan.retrofit2_download_file
 import okhttp3.ResponseBody
 import retrofit2.Call
 import retrofit2.http.GET
+import retrofit2.http.Streaming
 import retrofit2.http.Url
 
 
@@ -13,10 +14,12 @@ import retrofit2.http.Url
 interface ApiService {
 
     // option 1: a resource relative to your base URL
+    @Streaming
     @GET("/maven2/com/squareup/retrofit/retrofit/2.0.0-beta2/retrofit-2.0.0-beta2-javadoc.jar")
     fun downloadFileWithFixedUrl(): Call<ResponseBody>
 
     // option 2: using a dynamic URL
+    @Streaming
     @GET
     fun downloadFileWithDynamicUrlSync(@Url fileUrl: String): Call<ResponseBody>
 }
--- app/src/main/java/com/example/framgiatongxuanan/retrofit2_download_file/MainActivity.kt
+++ app/src/main/java/com/example/framgiatongxuanan/retrofit2_download_file/MainActivity.kt
@@ -1,12 +1,73 @@
 package com.example.framgiatongxuanan.retrofit2_download_file
 
+import android.content.Context
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import com.example.framgiatongxuanan.viblokolin.features.androidversion.Repository
+import okhttp3.ResponseBody
+import java.io.*
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+
+    }
+ fun downloadFile(){
+     Repository.createService(ApiService::javaClass).downloadFileWithFixedUrl
+ }
+
+    private fun writeResponseBodyToDisk(body: ResponseBody, fileName: String): Boolean {
+        try {
+            // todo change the file location/name according to your needs
+
+            var futureStudioIconFile = File(getExternalFilesDir(null).toString() + File.separator + fileName)
+            var inputStream: InputStream? = null
+            var outputStream: OutputStream? = null
+
+            try {
+                val fileReader = ByteArray(4096)
+
+                val fileSize = body.contentLength()
+                var fileSizeDownloaded: Long = 0
+
+                inputStream = body.byteStream()
+                outputStream = FileOutputStream(futureStudioIconFile)
+
+                while (true) {
+                    val read = inputStream!!.read(fileReader)
+
+                    if (read == -1) {
+                        break
+                    }
+
+                    outputStream!!.write(fileReader, 0, read)
+
+                    fileSizeDownloaded += read.toLong()
+
+                    Log.d("writeResponseBodyToDisk", "file download: $fileSizeDownloaded of $fileSize")
+                }
+
+                outputStream!!.flush()
+
+                return true
+            } catch (e: IOException) {
+                return false
+            } finally {
+                if (inputStream != null) {
+                    inputStream!!.close()
+                }
+
+                if (outputStream != null) {
+                    outputStream!!.close()
+                }
+            }
+        } catch (e: IOException) {
+            return false
+        }
+
     }
 }
